849eac78083aeb31b767802880ec3798015c41a0
Lucas Jaime <luuujaime@gmail.com>
2022-09-14 18:37:25 -0300

    Entrega 14/9

--- Javascript/app.js
+++ Javascript/app.js
@@ -3,3 +3,4 @@ let bebidas = ['tirano sour', 'my little pony', 'splinter', 'collins & dreams', 
 'swamp thing', 'george michael', 'jumanji', 'carrapicho tai', 'bowser', 'pantera rosa',
 'the inspector', 'la perla del atlantico', 'pinck tonic', 'grin tonic', 'steve', 'cormano', 'bob']
 alert(`En la consola se muestran los codigos con las bebidas, una vez visto que desea tomar, clickee el botón "Agregar al pedido"`)
+console.log(bebidas);
